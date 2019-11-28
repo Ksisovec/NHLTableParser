6bfa42dd83b1d864298793fd1dcde996f8a40cb5
ksisovec <ksisovec@gmail.com>
2019-11-28 20:10:03 +0300

    add db

--- src/main/java/model/BookValue.java
+++ src/main/java/model/BookValue.java
@@ -1,10 +1,17 @@
 package Model;
 
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 public class BookValues {
     Integer id;
     String bookName;
     String firstValue;
     String secondValue;
+    @ManyToOne
+    @JoinColumn(name = "")
+    private MatchResult matchResult;
+
     public BookValues(){}
 
     public BookValues(String bookName, String firstValue, String secondValue) {
